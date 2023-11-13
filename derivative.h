a0deabb83977452f0b2ce90bd5ed9ab4a93c0eb1
d33fur <d33fur@gmail.com>
2023-11-13 21:29:10 +0300

    rework

---
+++ derivative.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+namespace der {
+    double derivative(const std::string variableName, const std::string equationString, int orderOfDerivative, double point) {return 0;};
+    double derivative(const std::string variableName, const std::string equationString, double point) {return 0;};
+}
--- include/derivative.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace der {
-    double derivative(const std::string variableName, const std::string equationString, int orderOfDerivative, double point);
-    double derivative(const std::string variableName, const std::string equationString, double point);
-}
