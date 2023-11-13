fb6133282b403e8026260453431ad1f46bef2c83
d33fur <d33fur@gmail.com>
2023-11-13 23:37:35 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# course-work-numerical-methods
-Header-only C++ library for calculating Cauchy problem using numerical methods
+# 1. About crk4
+This is header-only C++ library for solving Cauchy problem using Runge-Kutta method.
+
+# 2. Usage
+Just add ```crk.h``` into your project.
--- crk4.h
+++ crk4.h
@@ -82,7 +82,3 @@ namespace crk {
             }; 
     };
 };
-
-
-    //double derivative(const std::string variableName, const std::string equationString, int orderOfDerivative, double point) {return 0;};
-    //double derivative(const std::string variableName, const std::string equationString, double point) {return 0;};
