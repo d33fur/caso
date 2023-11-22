977ecb8706173e822b2669afc3b8a1c077c537c6
Jung Dong Wook <jungdongwook@MacBook-Air-JDW.local>
2023-11-22 04:33:21 +0300

    some additions in test

--- tests/tests.py
+++ tests/tests.py
@@ -2,72 +2,78 @@ import cppyy
 import pytest
 import os
 
-headerFilePath = os.path.join(os.path.dirname(__file__), "caso.h")
+headerFilePath = os.path.join(f"{os.path.dirname(__file__)}", "caso.h")
 
-cppyy.include(headerFilePath)
+cppyy.include("/Users/jungdongwook/vscode/caso/caso.h")
+#cppyy.include(headerFilePath)
 
 caso = cppyy.gbl
 
-RungeKuttaTable = cppyy.gbl.std.vector[cppyy.gbl.std.vector[float]](
-    [
-        [0.],
-        [1. / 2., 1. / 2.],
-        [1. / 2., 0., 1. / 2.],
-        [1., 0., 0., 1.],
-        [1. / 6., 1. / 3., 1. / 3., 1. / 6.]
-    ]
-)
+#y-values initialization
+y = cppyy.gbl.std.vector[float] (
+    [2])
 
-DormanPrinceTable = cppyy.gbl.std.vector[cppyy.gbl.std.vector[float]](
-    [
-        [0.],
-        [1. / 5., 1. / 5.],
-        [3. / 10., 3. / 40., 9. / 40.],
-        [4. / 5., 44. / 45., -56. / 15., 32. / 9.],
-        [8. / 9., 19372. / 6561., -31760. / 2187., 10448. / 6561., 0., 25360. / 2187.],
-        [1., 9017. / 3168., -355. / 33., 46732. / 5247., 49. / 176., -5103. / 18656., 0., 5. / 143.],
-        [1. / 2., 35. / 384., 0., 500. / 1113., 125. / 192., -2187. / 6784., 11. / 84., 0.],
-        [35. / 384., 0., 500. / 1113., 125. / 192., -2187. / 6784., 11. / 84., 0.],
-        [71. / 57600., 0., -71. / 16695., 71. / 1920., -17253. / 339200., 22. / 525., -1. / 40]
-    ]
-)
+xl: float = 1.0 #left border
+xr: float = 3.0 #right border
+xs: float = 0.25 #step
+x: float = 0
 
-HeunTable = cppyy.gbl.std.vector[cppyy.gbl.std.vector[float]](
-    [
-        [0.],
-        [1., 1.],
-        [1. / 2., 1. / 2.],
-        [1., 0.],
-    ]
-)
+#equation initialization
+dydx = cppyy.gbl.std.vector[float] (
+    [(-2) * y[0] + x])
 
-FehilbergTable = cppyy.gbl.std.vector[cppyy.gbl.std.vector[float]](
-    [
-        [0.],
-        [1. / 4., 1. / 4.],
-        [3. / 8., 3. / 32., 9. / 32.],
-        [12. / 13., 1932. / 2197., -7200. / 2197., 7296. / 2197.],
-        [1., 439. / 216., -8., 3680. / 513., -845. / 4104.],
-        [1. / 2., -8. / 27., 2., -3544. / 2565., 1859. / 4104., -11. / 40.],
-        [25. / 216., 0., 1408. / 2565., 2197. / 4104., -1. / 5., 0.],
-        [-1. / 360., 0., 128. / 4275., 2197. / 75240., -1. / 50., -2. / 55.]
-    ]
-)
+def zeroStep(xs):
+    defNum = 1
+    xs = 0
+    returnDefVal(defNum, xs)
 
-BogackiShampineTable = cppyy.gbl.std.vector[cppyy.gbl.std.vector[float]](
-    [
-        [0.],
-        [1. / 2., 1. / 2.],
-        [3. / 4., 0., 3. / 4.],
-        [1., 2. / 9., 1. / 3., 4. / 9.],
-        [2. / 9., 1. / 3., 4. / 9., 0.],
-        [-7. / 72., 1. / 12., 1. / 9., -1. / 8.]
-    ]
-)
+def maxStep(xs):
+    defNum = 1
+    xs = 2
+    returnDefVal(defNum, xs)
+
+def LEquR(xr):
+    defNum = 2
+    xl = xr
+    returnDefVal(defNum, xs)
+
+def returnDefVal(defNum, value):
+    match defNum:
+        case 1:
+            xs = 0.25
+        case 2:
+            xl = 1.0
+            xr = 3.0
+
+a = caso.ODE(dydx, y, x, xr, xs)
+caso.setButcherTableau(caso.RungeKutta4)
+
+result = a.rungeKutta()
+
+expectedOutput = [0, 2]
+
+for i in range(len(result)):
+    print ("asdasdasdasd")
+    print (result[i])
 
 def testRungeKutta(capsys):
-    result = caso.rungeKutta
+    a = caso.ODE(dydx, y, x, xr, xs)
+    caso.setButcherTableau(caso.RungeKutta4)
 
-    expectedOutput = 
+    result = a.rungeKutta()
 
-    assert result == expectedOutput
+    expectedOutput = [0, 2]
+
+    for i in range(len(result)):
+        print ("asdasdasdasd")
+        print (result[i])
+
+    '''for i in range(len(expectedOutput)):
+        if expectedOutput[i] == result[i]:
+            continue
+        else:
+            
+
+    assert result == expectedOutput'''
+
+__
