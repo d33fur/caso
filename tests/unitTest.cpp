98f16b6ac7296fa473e5c843fce4d77e8d627780
Jung Dong Wook <jungdongwook@MacBook-Air-JDW.local>
2023-11-23 23:56:05 +0300

    test rebuild

---
+++ tests/unitTest.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../caso.h"
+#include <vector>
+#include <cmath>
+
+def getValues(){
+    std::vector<double> dydx;
+    double y, xl, xr, xs, x;
+}
+
+TEST_CASE("All tests passed", "[caso]") {
+    SECTION()
+}
